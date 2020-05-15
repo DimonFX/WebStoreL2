6518dc47b8ec2a8eb43b2dd3721c78a331e22ef0
DimonFX <50489975+DimonFX@users.noreply.github.com>
2020-05-15 12:59:47 +0500

    Базовый класс клиентов для Web-Api

---
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace WebStore.Clients.Base
+{
+    public abstract class BaseClient
+    {
+        protected readonly string _ServiceAddress;
+        protected readonly HttpClient _Client;
+        protected BaseClient(IConfiguration Configuration, string ServiceAddress)
+        {
+            _ServiceAddress = ServiceAddress;
+            _Client = new HttpClient
+            {
+                BaseAddress = new Uri(Configuration["WebApiURL"]),
+                DefaultRequestHeaders =
+                {
+                    Accept =
+                    {
+                        new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json")
+                    }
+                }
+            };
+
+        }
+    }
+}
