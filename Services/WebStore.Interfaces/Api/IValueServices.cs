a913ad394de6610fb1981b12d89c405a9f6999f1
DimonFX <50489975+DimonFX@users.noreply.github.com>
2020-05-15 12:27:38 +0500

    Интерфейс тестовой веб-службы

---
+++ Services/WebStore.Interfaces/Api/IValueServices.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace WebStore.Interfaces.Api
+{
+    public interface IValueServices
+    {
+        IEnumerable<string> Get();
+        string Get(int id);
+        /// <summary>
+        /// Добавляет элемент внутрь себя
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        Uri Post(string value);
+        /// <summary>
+        /// Обновляет элемент по id, возвращает статус код об успешности операции
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        HttpStatusCode Upt(int id, string value);
+        HttpStatusCode Delete(int id);
+
+    }
+}
