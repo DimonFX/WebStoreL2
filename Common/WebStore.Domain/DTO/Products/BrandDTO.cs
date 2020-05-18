1574279301df5b48950e70f0fdcfb9137ef3f752
DimonFX <50489975+DimonFX@users.noreply.github.com>
2020-05-18 15:23:45 +0500

    DTO модели для процесса передачи данных в WebAPI каталога товаров

---
+++ Common/WebStore.Domain/DTO/Products/BrandDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Domain.DTO.Products
+{
+    public class BrandDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
