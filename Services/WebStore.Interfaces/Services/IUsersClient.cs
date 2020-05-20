bf91224d92b64fdc4bb96f160c3cf5cf608763d6
DimonFX <50489975+DimonFX@users.noreply.github.com>
2020-05-20 11:45:12 +0500

    Клиент для WebApi системы Identity

--- Services/WebStore.Interfaces/Services/IUsersClient.cs
+++ Services/WebStore.Interfaces/Services/IUsersClient.cs
@@ -6,7 +6,7 @@ using WebStore.Domain.Entities.Identity;
 
 namespace WebStore.Interfaces.Services
 {
-    public interface IUserClient:
+    public interface IUsersClient:
         IUserRoleStore<User>,
         IUserPasswordStore<User>,
         IUserEmailStore<User>,
