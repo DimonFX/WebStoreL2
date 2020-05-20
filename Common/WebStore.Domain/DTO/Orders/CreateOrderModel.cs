db912fcb42a606ae5932429fc3724d9434cacaf9
DimonFX <50489975+DimonFX@users.noreply.github.com>
2020-05-20 15:54:14 +0500

    Комментарии к коду WebApi сервисов

--- Common/WebStore.Domain/DTO/Orders/CreateOrderModel.cs
+++ Common/WebStore.Domain/DTO/Orders/CreateOrderModel.cs
@@ -6,10 +6,19 @@ using WebStore.Domain.ViewModels.Orders;
 
 namespace WebStore.Domain.DTO.Orders
 {
+    /// <summary>
+    /// Структура оформляемого заказа
+    /// </summary>
     public class CreateOrderModel
     {
+        /// <summary>
+        /// Информация о заказе
+        /// </summary>
         public OrderViewModel OrderViewModel { get; set; }
 
+        /// <summary>
+        /// пункты закзаа
+        /// </summary>
         public List<OrderItemDTO> OrderItems { get; set; }
     }
 }
--- Common/WebStore.Domain/DTO/Orders/OrderDTO.cs
+++ Common/WebStore.Domain/DTO/Orders/OrderDTO.cs
@@ -5,14 +5,26 @@ using WebStore.Domain.Entities.Base;
 
 namespace WebStore.Domain.DTO.Orders
 {
+    /// <summary>
+    /// Структура заказа
+    /// </summary>
     public class OrderDTO : NamedEntity
     {
+        /// <summary>
+        /// Телефон для связи
+        /// </summary>
         public string Phone { get; set; }
-
+        /// <summary>
+        /// Адрес доставки
+        /// </summary>
         public string Address { get; set; }
-
+        /// <summary>
+        /// Дата формирования
+        /// </summary>
         public DateTime Date { get; set; }
-
+        /// <summary>
+        /// Пункты
+        /// </summary>
         public IEnumerable<OrderItemDTO> OrderItems { get; set; }
     }
 }
