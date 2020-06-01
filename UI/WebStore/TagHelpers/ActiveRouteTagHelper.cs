6c3cf5dc9263885175c8a6f80598e881dc99810a
DimonFX <50489975+DimonFX@users.noreply.github.com>
2020-06-01 12:54:52 +0500

    логика подсветки активных ссылок таг хелпера

--- UI/WebStore/TagHelpers/ActiveRouteTagHelper.cs
+++ UI/WebStore/TagHelpers/ActiveRouteTagHelper.cs
@@ -11,7 +11,11 @@ namespace WebStore.TagHelpers
     [HtmlTargetElement(Attributes = AttributeName)]
     public class ActiveRouteTagHelper:TagHelper
     {
+
+
         const string AttributeName = "is-active-route";
+        private const string IgnoreActionName = "ignore-action";
+
         [HtmlAttributeName ("asp-action")]
         public string Action { get; set; }
         [HtmlAttributeName("asp-controller")]
@@ -24,8 +28,53 @@ namespace WebStore.TagHelpers
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            var is_ignore_action = context.AllAttributes.ContainsName(IgnoreActionName);
+
+            if (IsActive(is_ignore_action))
+                MakeActive(output);
+
             output.Attributes.RemoveAll(AttributeName);
-            //base.Process(context, output);
+            output.Attributes.RemoveAll(IgnoreActionName);
+        }
+
+        private void MakeActive(TagHelperOutput output)
+        {
+            var class_attribute = output.Attributes.FirstOrDefault(attr => attr.Name == "class");
+
+            if (class_attribute is null)
+                output.Attributes.Add("class", "active");
+            else
+            {
+                if (class_attribute.Value.ToString()?.Contains("active") ?? false) return;
+
+                output.Attributes.SetAttribute("class",
+                    class_attribute.Value is null
+                        ? "active"
+                        : class_attribute.Value + " active");
+            }
+        }
+
+        private bool IsActive(bool IsIgnoreAction)
+        {
+            var route_values = ViewContext.RouteData.Values;
+            //Определяем какой был запрошен контроллер
+            var current_controller = route_values["controller"].ToString();
+            //Определяем какое действие было запрошено
+            var current_action = route_values["action"].ToString();
+
+            const StringComparison ignore_case = StringComparison.OrdinalIgnoreCase;
+            if (!string.IsNullOrEmpty(Controller)&& !string.Equals(current_controller,Controller, ignore_case))
+                return false;
+
+            if (!string.IsNullOrEmpty(Action) && !string.Equals(current_action, Action, ignore_case))
+                return  false;
+
+            foreach (var (key,value) in RouteValues)
+            {
+                if (!route_values.ContainsKey(key) || route_values[key].ToString() != value)
+                    return false;
+            }
+            return true;
         }
     }
 }
